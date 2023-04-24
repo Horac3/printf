c6c548468ce61fcd4d1f3cab6f2e9200c0b6edac
Horac3 <lwandahorace@gmail.com>
2023-04-24 09:47:17 -0700

    3 initial files

---
+++ _printf.c
@@ -0,0 +1,53 @@
+#include <stdarg.h>
+#include <unistd.h>
+#include "main.h"
+
+
+/**
+ * _printf - produces output according to a format.
+ * @format: A string containing zero or more directives
+ *
+ * Return: The number of characters printed (excluding the null byte used to end output to strings)
+ */
+int _printf(const char *format, ...)
+{
+	va_list args;
+	int i, count = 0;
+
+	va_start(args, format);
+
+	for (i = 0; format[i] != '\0'; i++)
+	{
+		if (format[i] == '%')
+		{
+			i++;
+			switch (format[i])
+			{
+				case 'c':
+					_putchar(va_arg(args, int));
+					count++;
+					break;
+				case 's':
+					count += write(1, va_arg(args, char *), 0);
+					break;
+				case '%':
+					_putchar('%');
+					count++;
+					break;
+				default:
+					_putchar('%');
+					_putchar(format[i]);
+					count += 2;
+					break;
+			}
+		}
+		else
+		{
+			_putchar(format[i]);
+			count++;
+		}
+	}
+	va_end(args);
+	return (count);
+}
+
