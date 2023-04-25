1603ccc849edbb63a620649431e311db1edf22db
Mbechi <agnessmminaa@gmail.com>
2023-04-25 13:04:03 -0700

    mbechi

--- main.h
+++ main.h
@@ -13,7 +13,5 @@ int print_unsigned(unsigned int n);
 int print_octal(unsigned int n);
 int print_hex(unsigned int n, int uppercase);
 int print_binary(unsigned int n);
-int handle_print(const char *fmt, int *ind, va_list list, char buffer[],
-        int flags, int width, int precision, int size)
 
 #endif /* MAIN_H */
